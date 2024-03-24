fecaba6d4ce413b099150c06885530989eb8fac7
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-23 19:52:19 -0600

    Secrets testing

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const { Client } = require("discord.js")
 
 const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"]})
 
-client.login()
+console.log(secrets.BOT_TOKEN)
+
+client.login(secrets.BOT_TOKEN)
