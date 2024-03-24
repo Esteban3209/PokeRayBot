8ea0c2ad76f8d9716aef99c777b195fa3eec4b25
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-23 22:36:19 -0600

    Updated index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Client } = require("discord.js")
+const { Client, Partials } = require("discord.js")
 
-const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"]})
+const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"], partials: [  ]})
 
-console.log(process.env.BOT_TOKEN)
+console.log(Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User)
 
 client.login(process.env.BOT_TOKEN)
