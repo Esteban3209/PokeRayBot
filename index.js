b04b78e4a135ecbb08f3f07124db707726749737
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-23 21:50:18 -0600

    Secrets testing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { Client } = require("discord.js")
+require("dotenv").config()
 
 const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"]})
 
-console.log(secrets.BOT_TOKEN)
+console.log(process.env.BOT_TOKEN)
 
-client.login(secrets.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
