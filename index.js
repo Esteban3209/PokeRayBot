005c475fb3f20b1c75904a0319f813b2e3565bf2
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-23 19:32:54 -0600

    Bot login

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const { Client, Intents } = require("discord.js")
+const { Client } = require("discord.js")
 
-const client = new Client({})
+const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"]})
+
+client.login()
