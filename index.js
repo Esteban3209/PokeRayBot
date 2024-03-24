255d1ea0e49c544e412b151b9410b66b8865ac49
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-24 10:28:29 -0600

    Database testing

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const axios = require("axios")
 const client = new Client({intents: ["GuildBans", "GuildIntegrations", "GuildInvites", "GuildMembers", "GuildMessageReactions", "GuildMessages", "GuildModeration", "GuildPresences", "GuildScheduledEvents", "GuildVoiceStates", "Guilds", "MessageContent"], partials: [ 0, 1, 2, 3, 4 ]})
 
 async function getData(url) {
-    var res = await axios.get(url, {
-        headers: {
-            'Authorization': '55de8c36-bdca-4083-8908-affc0845aacc'
-        }
-    })
-    console.log(await res)
+    var res = await axios.get(url)
+    return res.data
 }
 
-getData('https://api.jsonstorage.net/v1/json/645c2fc7-bc7a-4e9a-9ea8-78beb159b036/cdf8dad0-fbad-4bc8-8d22-893f76670b5a?apiKey=5f929d3c-38ea-4de5-bc8f-43363d6a3d46')
+console.log(getData(`https://api.jsonstorage.net/v1/json/645c2fc7-bc7a-4e9a-9ea8-78beb159b036/cdf8dad0-fbad-4bc8-8d22-893f76670b5a?apiKey=${process.env.API_KEY}`))
 
 client.login(process.env.BOT_TOKEN)
