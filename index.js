ab43170a23ecb0d3a04c91e27e0619741fb93a51
Esteban Izaguirre <estebanizaguirreh@hotmail.com>
2024-03-24 10:17:42 -0600

    Database testing

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ async function getData(url) {
             'Authorization': '55de8c36-bdca-4083-8908-affc0845aacc'
         }
     })
-    return res
+    console.log(await res)
 }
 
-console.log(getData('https://api.jsonstorage.net/v1/json/645c2fc7-bc7a-4e9a-9ea8-78beb159b036/cdf8dad0-fbad-4bc8-8d22-893f76670b5a?apiKey=5f929d3c-38ea-4de5-bc8f-43363d6a3d46'))
+getData('https://api.jsonstorage.net/v1/json/645c2fc7-bc7a-4e9a-9ea8-78beb159b036/cdf8dad0-fbad-4bc8-8d22-893f76670b5a?apiKey=5f929d3c-38ea-4de5-bc8f-43363d6a3d46')
 
 client.login(process.env.BOT_TOKEN)
